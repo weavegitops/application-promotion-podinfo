277ee00b2cb45604fbe2f441f8e48c84de236f52
Darryl Weaver <darryl@weave.works>
2022-09-27 11:21:21 +0100

    edit the demo script

--- demo/01-edit-container-and-increment-versions.sh
+++ demo/01-edit-container-and-increment-versions.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+echo "Here are links to Demo2 cluster environments for the podinfo app:"
+echo "dev - http://172.16.20.211/podinfo"
+echo "dev-test - http://172.16.20.212/podinfo"
+echo "uat - http://172.16.20.213/podinfo"
+echo "stg - http://172.16.20.214/podinfo"
+echo "prod - http://172.16.20.215/podinfo"
+echo "Open browser windows to each of these to show the progression during the demo."
+echo
+echo "Press a Key"
+echo
+read -n1 -s
+echo "To start we are going to create a git repo feature branch to develop our changes to the conatiner code."
+git checkout main
+git pull
+git checkout -b demo-podinfo-updates
+echo
+echo "Now we have a feature branch, we can start making changes to the container code"
+echo
 echo "We are going edit the container code to set the color of the background of the podinfo app"
 echo "The default background color is set here:"
 echo
@@ -16,6 +34,18 @@ fi
 echo "Now the color is changed and it is set like this:"
 grep ui-color ../cmd/podinfo/main.go
 echo
+echo "We add, commit and push the changes to the repository feature branch"
+echo
+git add ../cmd/podinfo/main.go
+git commit -m 'change the background colour'
+git push --set-upstream origin demo-podinfo-updates
+echo
+echo "Pause here to show the podinfo app on the dev cluster"
+echo "This will show the colour change in the new container version, to give fast feedback to the developer."
+echo
+echo "Press a key to continue"
+read -n1 -s
+echo
 echo "We are also going to increment the version of the application, it is currently"
 VERSION=$(grep VERSION ../pkg/version/version.go | sed -e 's/var VERSION = "//' -e 's/"//')
 echo "$VERSION"
@@ -31,11 +61,12 @@ echo
 echo "Now it looks like this:"
 cat ../pkg/version/version.go
 echo
+read -n1 -s
 echo "We also need to set the version of the container in the helm chart values"
 yq eval '.podinfo.image.tag=env(NEXTVERSION)' -i ../charts/podinfo/values.yaml
 echo "Container tag set in the helm chart to ${NEXTVERSION}"
 echo
-echo "Then we increment the version of the helm chart to relase the new Helm chart with the new podinfo container"
+echo "Then we increment the version of the helm chart to release the new Helm chart with the new podinfo container"
 HELM_VERSION=$(yq .version ../charts/podinfo/Chart.yaml)
 echo "Current Helm chart version: $HELM_VERSION"
 export HELM_NEXT=$(echo ${HELM_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
@@ -45,31 +76,47 @@ echo
 echo "Next we create the git repo branch and push to the repo"
 read -n1 -s
 echo "git add and commit changes"
-git checkout -b demo-podinfo-updates
 git add ../
 git commit -m "Update container to version ${NEXTVERSION} and Helm chart to version ${HELM_NEXT}"
 echo "git push changes to the repository"
-git push --set-upstream origin demo-podinfo-updates
-git checkout main
-git branch -d demo-podinfo-updates
+git push
 echo
+echo "Now we need to create a PR for the feature request, once a PR is created the tests will run in CI"
+echo "Go and create a PR here: https://github.com/weavegitops/application-promotion-podinfo/pulls"
+echo "Then show the tests running here: https://github.com/weavegitops/application-promotion-podinfo/actions"
 echo
+echo "Press a key"
+read -n1 -s
+echo "Once all the tests have passed, it is time to merge the PR to release the Helm chart"
+echo "Merge the PR here: https://github.com/weavegitops/application-promotion-podinfo/pulls"
+echo
+echo "Press a Key"
+echo
+read -n1 -s
 echo "============================================================================================================="
-echo "The dev and dev-test environment will receive this change automatically"
+echo "Once the PR is merged, this will run the Helm chart release process and make the new chart available for deployment."
+echo "The dev-test environment will receive this change automatically once the release is completed"
+echo "Now show the podinfo app on the dev-test cluster to see it deployed."
+echo
+echo "Press a Key"
+echo
+read -n1 -s
 echo "To promote this to the UAT environment:"
-echo "Create a PR to main from the branch demo-podinfo-updates in the github repo here:"
-echo "https://github.com/weavegitops/application-promotion-podinfo"
+echo "Approve the PR to deploy to UAT in the github repo here:"
+echo "https://github.com/weavegitops/application-promotion-podinfo/pulls"
 echo 
-echo "The PR created will then run the CI testing."
-echo "When this passes the tests, merge the change into main"
-echo "This will trigger the Helm release process and promote across the environments via PRs you have to approve."
-echo "So, watch the progress of the app using the podinfo links below and show the progression in the Weave Gitops UI"
-echo "Then approve the next PR, to progress."
-echo "To display the progress open 5 browser windows for each of the environments:"
-echo "Demo2 links:"
-echo "dev - http://172.16.20.211/podinfo"
-echo "dev-test - http://172.16.20.212/podinfo"
-echo "uat - http://172.16.20.213/podinfo"
-echo "stg - http://172.16.20.214/podinfo"
-echo "prod - http://172.16.20.215/podinfo"
-
+echo "Press a Key"
+echo
+read -n1 -s
+echo "To promote this to the stg environment:"
+echo "Approve the PR to deploy to stg in the github repo here:"
+echo "https://github.com/weavegitops/application-promotion-podinfo/pulls"
+echo 
+echo "Press a Key"
+echo
+read -n1 -s
+echo "To promote this to the prod environment:"
+echo "Approve the PR to deploy to prod in the github repo here:"
+echo "https://github.com/weavegitops/application-promotion-podinfo/pulls"
+echo 
+echo "The demo is now completed"
